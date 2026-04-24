4d8321e94a61e6b3f7acbbc787672e483759a589
赵坤 <2926778293@qq.com>
2026-04-24 17:50:30 +0800

    修改配置文件

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
-  base: '/pomodoro-app1/',
   plugins: [
     vue(),
     WindiCSS(),
